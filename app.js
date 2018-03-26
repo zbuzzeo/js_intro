34297502f7ddd74c8fb9b58d8ad6df42689fe471
Victor <viclee02@gmail.com>
2018-03-25 18:34:21 -1000

    adds new exercises

---
+++ app.js
@@ -0,0 +1,43 @@
+//1. Comments are used to explain code and used to prevent execution.
+// For a single line comment use //
+// For mulitple line comments use /* */
+
+// Add a single line comment next to the code below:
+
+var today = "Monday";
+
+// Next, using the multiple line comments, comment out the following code:
+
+function commentOut(str){
+ console.log(str + " is a great day!");
+}
+
+/*2. Declare a variable named peanuts and assign it with your favorite Peanuts character. 
+Console.log the variable.*/
+
+/*3. Declare a variable named snoopyAge and assign it with Snoopy's real age.
+Console.log the variable.*/
+
+/*4. Declare a variable named isCharlieBrownHip and assign it with a boolean value of your choice.
+Console.log the variable.*/
+
+/*5. Declare a variable named peanutsGang and assign it with an array of 5 of Peanuts characters.
+Console.log the variable.
+Console.log only the first element in the array
+Console.log only the last element in the array
+Console.log the length of the arrray*/
+
+/*6. Declare a variable named peanutsEndDate and assign it with null data type. 
+Console.log the variable.*/
+
+/*7. Declare a variable named phrase that will concatenate the following variables below to form a sentence that will read:
+'Charlie Brown you are a blockhead!';
+Console.log the variable*/
+
+var character = "Charlie Brown";
+var quote = "you are a blockhead!";
+
+/*8. Declare a variable named myFav that will concatenate your favorite Peanuts character (#2 above) to form a sentence that will read:
+'My favorite Peanuts character is xxxxxx.'
+Console.log the variable.*/
+
